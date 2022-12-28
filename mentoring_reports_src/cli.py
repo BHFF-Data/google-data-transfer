b8371a789ac6318586cb63ab874be1a83ff15ac4
nake-b <basicnadir@gmail.com>
2022-12-28 16:53:20 +0100

    Initial branch commit

--- mentoring_reports_src/cli.py
+++ mentoring_reports_src/cli.py
@@ -1,13 +1,22 @@
 import gin
 import typer
-from mentoring_reports_src.controller import download_responses
+from mentoring_reports_src.controller import load_google_data
+from mentoring_reports_src.data_transfer import match_form_with_sheet
 
 
-def main(form_url: str, sheet_url: str, sheet_name: str):
+def parse_args(form_url: str, sheet_url: str, sheet_name: str):
     form_id = form_url.split("/")[-2]
     sheet_id = sheet_url.split("/")[-2]
     sheet_name = sheet_name.replace("_", " ")
-    download_responses(form_id, sheet_id, sheet_name)
+    return form_id, sheet_id, sheet_name
+
+
+def main(form_url: str, sheet_url: str, sheet_name: str):
+    form_id, sheet_id, sheet_name = parse_args(form_url, sheet_url, sheet_name)
+    form_df, sheet_df = load_google_data(form_id, sheet_id, sheet_name)
+    print("Connected to Google Api")
+    res = match_form_with_sheet(form_df, sheet_df)
+    print(res)
 
 
 if __name__ == "__main__":
--- mentoring_reports_src/controller.py
+++ mentoring_reports_src/controller.py
@@ -2,6 +2,8 @@ import os
 from typing import Union
 
 import gin
+import pandas as pd
+
 from mentoring_reports_src.dao.forms_dao import GoogleFormsApiDAO
 from mentoring_reports_src.dao.sheets_dao import GoogleSheetsApiDAO
 from mentoring_reports_src.google_api.auth import authenticate_google_api
@@ -9,30 +11,37 @@ from mentoring_reports_src.google_api.google_connection import (GoogleFormsConne
                                                                 GoogleSheetsConnection)
 
 
-@gin.configurable
-def download_responses(
-    form_id: str,
-    sheet_id: str,
-    sheet_name: str,
-    forms_datapath: Union[str, os.PathLike],
-    sheets_datapath: Union[str, os.PathLike],
-) -> None:
-
+def connect_to_google_api():
     creds = authenticate_google_api()
     forms_conn = GoogleFormsConnection(creds)
     sheets_conn = GoogleSheetsConnection(creds)
+    return forms_conn, sheets_conn
 
+
+@gin.configurable
+def load_google_data(
+        form_id: str,
+        sheet_id: str,
+        sheet_name: str,
+        forms_save_datapath: Union[str, os.PathLike] = None,
+        sheets_save_datapath: Union[str, os.PathLike] = None,
+) -> (pd.DataFrame, pd.DataFrame):
+    forms_conn, sheets_conn = connect_to_google_api()
     forms_dao = GoogleFormsApiDAO(google_forms_conn=forms_conn, forms_id=form_id)
     sheets_dao = GoogleSheetsApiDAO(
         google_sheets_conn=sheets_conn, sheet_id=sheet_id, sheet_name=sheet_name
     )
 
-    df = sheets_dao.load_sheet_data()
-    sheets_save_path = os.path.join(sheets_datapath, f"sheet_{sheet_id}.csv")
-    df.to_csv(sheets_save_path)
-    print(f"Saved {sheet_id} to {sheets_save_path}")
+    sheets_df = sheets_dao.load_sheet_data()
+    if sheets_save_datapath is not None:
+        sheets_save_path = os.path.join(sheets_save_datapath, f"sheet_{sheet_id}.csv")
+        sheets_df.to_csv(sheets_save_path)
+        print(f"Saved {sheet_id} to {sheets_save_path}")
 
-    df = forms_dao.load_form_data()
-    save_path = os.path.join(forms_datapath, f"responses_{form_id}.csv")
-    df.to_csv(save_path)
-    print(f"Saved {form_id} to {save_path}")
+    forms_df = forms_dao.load_form_data()
+    if forms_save_datapath is not None:
+        save_path = os.path.join(forms_save_datapath, f"responses_{form_id}.csv")
+        forms_df.to_csv(save_path)
+        print(f"Saved {form_id} to {save_path}")
+
+    return forms_df, sheets_df
