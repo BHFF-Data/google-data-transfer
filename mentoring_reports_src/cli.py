17d752a45c9bca830b410f64e6f5a27273a51894
nake-b <basicnadir@gmail.com>
2023-01-16 20:42:46 +0100

    Mass refactor + sheet writing

--- mentoring_reports_src/cli.py
+++ mentoring_reports_src/cli.py
@@ -1,33 +1,13 @@
 import gin
 import typer
-from mentoring_reports_src.controller import (
-    connect_to_google_api,
-    save_google_data,
-    transfer_form_responses_to_sheet,
-)
-from mentoring_reports_src.dao.file_system_dao import FileDAO
-from mentoring_reports_src.dao.google_api_dao import GoogleAPIDao
+from mentoring_reports_src.controller import main
 
 
-def parse_args(form_url: str, sheet_url: str, sheet_name: str):
-    form_id = form_url.split("/")[-2]
-    sheet_id = sheet_url.split("/")[-2]
-    sheet_name = sheet_name.replace("_", " ")
-    return form_id, sheet_id, sheet_name
-
-
-def main(form_url: str, sheet_url: str, sheet_name: str, local: bool = False):
-    form_id, sheet_id, sheet_name = parse_args(form_url, sheet_url, sheet_name)
-    forms_conn, sheets_conn = connect_to_google_api()
-    if local:
-        dao = FileDAO()
-    else:
-        dao = GoogleAPIDao(forms_conn, sheets_conn)
-    form_df, sheet_df = save_google_data(form_id, sheet_id, sheet_name, dao)
-    new_sheet_df = transfer_form_responses_to_sheet(form_df, sheet_df)
-    print(new_sheet_df)
+def cli_main(form_url: str, sheet_url: str, sheet_name: str):
+    sheet_name = sheet_name.replace("_", " ")  # typer can't accept strings with a space
+    main(form_url, sheet_url, sheet_name)
 
 
 if __name__ == "__main__":
     gin.parse_config_file("config/local.gin")
-    typer.run(main)
+    typer.run(cli_main)
---
+++ mentoring_reports_src/google_api/sheet.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+
+import gspread
+import pandas as pd
+from mentoring_reports_src.commons import PathType
+
+
+class Sheet(ABC):
+    def __init__(self, spreadsheet_url: str, sheet_name: str):
+        self.spreadsheet_url = spreadsheet_url
+        self.spreadsheet_id = spreadsheet_url.split("/")[-2]
+        self.name = sheet_name
+
+    @abstractmethod
+    def to_df(self) -> pd.DataFrame:
+        ...
+
+    @abstractmethod
+    def write_col(self, col_name: str, col_values: list) -> None:
+        ...
+
+
+class GSpreadSheet(Sheet):
+    def __init__(self, spreadsheet_url: str, sheet_name: str, creds_path: PathType):
+        super().__init__(spreadsheet_url, sheet_name)
+        self.creds_path = creds_path
+        self._gc = gspread.oauth(credentials_filename=creds_path)
+        self._spreadsheet = self._gc.open_by_url(spreadsheet_url)
+        self._sheet = self._spreadsheet.worksheet(sheet_name)
+        self._df = self._to_df()
+
+    def find_cells(self, query: str) -> list[gspread.Cell]:
+        return self._sheet.findall(query)
+
+    def _to_df(self) -> pd.DataFrame:
+        return pd.DataFrame(self._sheet.get_all_records())
+
+    def to_df(self) -> pd.DataFrame:
+        return self._df
+
+    def write_col(self, col_name: str, col_values: list) -> None:
+        col_header_cells = self.find_cells(col_name)
+        if len(col_header_cells) != 1:
+            raise ValueError(
+                f"Found {len(col_header_cells)} cells with name {col_name}."
+            )
+        col_header_cell = col_header_cells.pop()
+        # Compute range in A1 notation
+        col_a1 = col_header_cell.address.replace(str(col_header_cell.row), "")
+
+        first_row_a1 = str(col_header_cell.row + 1)
+        first_cell = col_a1 + first_row_a1
+
+        num_rows = len(self._df)
+        last_row_a1 = str(num_rows + 1)
+        last_cell = col_a1 + last_row_a1
+
+        update_range = first_cell + ":" + last_cell
+        # Process col_values for gspread update
+        update_values = [[el] for el in col_values]
+
+        self._sheet.update(update_range, update_values)
