c390ed5bc5874bb1fd5c0fda29e39a9c4e409048
nake-b <basicnadir@gmail.com>
2023-01-31 18:25:54 +0100

    Refactor, update mentoring reports transfer config

--- mentoring_reports_src/transfer_configs/mentoring_reports.py
+++ mentoring_reports_src/transfer_configs/mentoring_reports.py
@@ -4,26 +4,49 @@ import pandas as pd
 from mentoring_reports_src.commons import PathType
 from mentoring_reports_src.transfer_configs.transfer_config import TransferConfig
 
-ACTIVITY_QUESTION = "Have you made the inital contact with your mentor?"
-ACTIVITY_MAP = {"Yes": "Active", "No": "Inactive"}
+ACTIVITY_QUESTION = "Do you have recommended number of meetings with your mentor?"
+INACTIVITY_REASON_QUESTION = (
+    "If the answer to the previous question is 'No', please state why"
+)
+
+INACTIVITY_VALUE = "Inactive"
+ACTIVITY_MAP = {"Yes": "Active", "No": INACTIVITY_VALUE}
+INACTIVITY_MAP = {
+    "I wasn't available for my mentor": "Inactive due to mentee",
+    "My Mentor wasn't available for me": "Inactive due to mentor",
+}
+
 COLUMNS_JOIN_MAP = {
     "First and Last Name": "Mentee",
     "Mentor's First and Last Name": "Mentor",
 }
-TARGET_COL = "Status - July 2023"
+TARGET_COL = "Status - January 2023"
 DEFAULT_NAME = "Mentoring Reports Default"
 
 
 def compute_activity_col(
     form_df: pd.DataFrame,
     activity_question: str = ACTIVITY_QUESTION,
-    activity_response_map: str = ACTIVITY_MAP,
+    activity_response_map: dict[str:str] = ACTIVITY_MAP,
+    inactivity_question: str = INACTIVITY_REASON_QUESTION,
+    inactivity_response_map: dict[str:str] = INACTIVITY_MAP,
+    inactivity_value: str = INACTIVITY_VALUE,
     columns_join_map: dict[str:str] = COLUMNS_JOIN_MAP,
 ) -> pd.DataFrame:
+    df = form_df
+    df[activity_question] = df[activity_question].replace(activity_response_map)
+
+    if inactivity_question in form_df.columns:
+        df[inactivity_question] = df[inactivity_question].replace(
+            inactivity_response_map
+        )
+        df.loc[df[activity_question] == inactivity_value, activity_question] = df.loc[
+            df[activity_question] == inactivity_value, inactivity_question
+        ]
+
     form_match_cols = list(columns_join_map.keys())
     form_cols = form_match_cols + [activity_question]
-    df = form_df[form_cols]
-    df[activity_question] = df[activity_question].replace(activity_response_map)
+    df = df[form_cols]
     df.rename(columns={activity_question: "target"}, inplace=True)
     return df
 
