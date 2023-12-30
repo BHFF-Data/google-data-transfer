d06e924f3fa1f3de6cc3575fb66a64417b4bbc76
nihad-hh <hodzicnihad10@gmail.com>
2023-12-30 19:55:59 +0100

    Updated as per 25.12 meeting

--- webapp_run.py
+++ webapp_run.py
@@ -1,30 +1,5 @@
 import gin
-import streamlit as st
-
-from google_data_transfer.controller.webapp import WebApp
-
-
-def main():
-    webapp = WebApp()
-
-    st.title("Mentoring Reports")
-
-    form_url = st.text_input("Unesite 'edit' link Google forme:", key="form")
-    if form_url:
-        webapp.init_form_env(form_url, st.secrets["token"])
-        sheet_url = st.text_input("Unesite 'edit' link Google sheeta:")
-        sheet_name = st.text_input("Unesite ime sheeta:")
-        if sheet_url and sheet_name:
-            webapp.init_sheet_env(sheet_url, sheet_name, st.secrets["installed"])
-            cols = webapp.get_sheet_cols()
-            target_col = st.selectbox(
-                "Izaberite u koju kolonu želite upisati rezultat:", cols, index=None
-            )
-            if target_col:
-                with st.spinner(text="Transfer u toku..."):
-                    webapp.transfer(target_col)
-                st.success("Transfer uspješan!")
-
+from google_data_transfer.ui.webapp import main
 
 if __name__ == "__main__":
     gin.parse_config_file("config/local.gin")
