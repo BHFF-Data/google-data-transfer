7b16cb3c949dfb1a1d72af22a160a834bd5b098a
nihad-hh <hodzicnihad10@gmail.com>
2024-01-20 11:03:12 +0100

    Added libraries for deployment

--- google_auth.py
+++ google_auth.py
@@ -1,19 +1,8 @@
-import os.path
-
-
-from google.oauth2.service_account import Credentials
-from google_data_transfer.google_api.google_client import GoogleFormsClient
-from google_data_transfer.google_api.form import GoogleForm
-
 # tutorial
 
-import pandas as pd
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
-from httplib2 import Http
-from apiclient import discovery
+
+from oauth2client.service_account import ServiceAccountCredentials
 
 creds_token_path = "secrets/credentials.json"
 creds_path = "./secrets/credentials.json"
@@ -29,10 +18,8 @@ scopes = (
 )
 form_url = "https://docs.google.com/forms/d/1Yal1J9Tgg1Men5MYdD3eT7kjCYaXZhOsuoYSpQU1MU4/edit"
 
-
 from apiclient import discovery
 from httplib2 import Http
-from oauth2client import client, file, tools
 
 SCOPES = "https://www.googleapis.com/auth/forms.responses.readonly"
 DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
@@ -51,12 +38,13 @@ def authenticate():
 
     scopes = ['https://www.googleapis.com/auth/forms.responses.readonly']
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(
-       keyfile_dict=st.secrets["service_account"], scopes=scopes)
+        keyfile_dict=st.secrets["service_account"], scopes=scopes)
     http = credentials.authorize(Http())
-    service = discovery.build('forms', 'v1', http=http, discoveryServiceUrl=DISCOVERY_DOC, static_discovery=False,)
+    service = discovery.build('forms', 'v1', http=http, discoveryServiceUrl=DISCOVERY_DOC, static_discovery=False, )
     form_id = "1ver4nYOU3mrigFg_ly9f2_HwHvdCKmqQLrR5IXZDGaQ"
     result = service.forms().responses().list(formId=form_id).execute()
     st.write(result["responses"][0])
+
 
 if __name__ == "__main__":
     authenticate()
